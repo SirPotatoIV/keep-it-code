7f8070fd9e7e0e1cd89d8012edee8afe75705e39
SirPotatoIV <otoolej33@gmail.com>
2020-02-20 23:14:26 -0600

    Subscribe page now adds subscribers to the mailing list.

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,5 +1,8 @@
-// const nodemailer = require("nodemailer")
+/*global axios*/
+/*eslint no-undef: "error"*/
+/*eslint-env browser*/
 
+// const nodemailer = require("nodemailer")
 // let transporter = nodemailer.createTransport(transport[,defaults])
 
 const subscribeBtnEl = document.getElementById("subscribeBtn");
@@ -7,11 +10,21 @@ const firstNameInputEl = document.getElementById("firstNameInput");
 const lastNameInputEl = document.getElementById("lastNameInput");
 const emailInputEl = document.getElementById("emailInput");
 
-subscribeBtnEl.addEventListener("click", function(event) {
+subscribeBtnEl.addEventListener("click", async function(event) {
   event.preventDefault();
   console.log(
     emailInputEl.value,
     firstNameInputEl.value,
     lastNameInputEl.value
   );
+  try {
+    const response = axios.post("/api/subscribers", {
+      first_name: firstNameInputEl.value,
+      last_name: lastNameInputEl.value,
+      email: emailInputEl.value
+    });
+    console.log(response);
+  } catch (error) {
+    console.log("error trying to add a new subscriber");
+  }
 });
